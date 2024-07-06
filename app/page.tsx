631e0970b342e9abd1b804cc97c5d7f6244adaf8
Avnish Kumar <88883963+acko-avnish@users.noreply.github.com>
2024-07-06 16:44:36 +0530

    Basic set-up done for the react project

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+import PaitentForms from "@/components/forms/PaitentForms";
 import Image from "next/image";
+
 
 export default function Home() {
   return (
-   <div>
-     <h1 className="text-3xl text-red-500 underline font-bold"> Home Landing Page </h1>
+   <div className="flex h-screen max-h-screen">
+     <section className="remove-scrollbar container my-auto">
+      <div className="sub-container max-w-[496px]">
+        <Image 
+        src="/assets/icons/logo-full.svg"
+        alt="patient"
+        height={1000}
+        width={1000}
+        className="mb-12 h-10 w-fit"   
+        />
+        <h1 className="text-white"> Hello </h1>
+        < PaitentForms/>
+      </div>
+     </section>
    </div>
   );
 }
