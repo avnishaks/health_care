0122813e766f71aba48688ad885d13f2e8b0eecc
Avnish Kumar <88883963+acko-avnish@users.noreply.github.com>
2024-07-07 17:47:28 +0530

    e2e flow of careplus done

--- components/forms/AppointmentForms.tsx
+++ components/forms/AppointmentForms.tsx
@@ -84,7 +84,7 @@ export const AppointmentForm = ({
         if (newAppointment) {
           form.reset();
           router.push(
-            `/patients/${userId}/new-appointment/success?appointmentId=${newAppointment.$id}`
+            `/patient/${userId}/new-appointment/success?appointmentId=${newAppointment.$id}`
           );
         }
       } else {
